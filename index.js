e0ba9daf89f2e6a03124c10f925c4fc9d367ff2c
diduladishan <diduladishan14@gmail.com>
2025-02-16 13:29:32 +0530

    feat: creating a web server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //Reading and writing files
-const fs = require("fs");
+// const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Wriiten");
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Wriiten");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
